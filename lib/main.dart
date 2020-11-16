e297fca4c154bf8028c2aa9ff76ea885d4c3e425
Paula Sandoli <paulasandoli@gmail.com>
2020-11-16 20:12:15 -0300

    Login front

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,116 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aluguel de Imóveis',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Aluguel de Imóveis'),
-        ),
-        body: Center(
-          child: Text('Login'),
-        ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(title: 'Login'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextStyle style = TextStyle(fontFamily: 'Trajan Pro', fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final emailField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          fillColor: Color(0xFFFFFFFF),
+          filled: true,
+          contentPadding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+          hintText: "Email",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(5.0))),
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+        fillColor: Color(0xFFFFFFFF),
+        filled: true,
+        contentPadding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+        hintText: "Senha",
+        border:
+        OutlineInputBorder(borderRadius: BorderRadius.circular(5.0))),
+    );
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(5.0),
+      color: Color(0xFFFFE3B3),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 25.0),
+        onPressed: () {},
+        child: Text("ENTRAR",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Color(0xFF26648E), fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.4, 0.9],
+            colors: [
+              Color(0xFF26648E),
+              Color(0xFF53D2DC),
+            ],
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: Center(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(36.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 155.0,
+                        child: Image.asset(
+                          "logo.png",
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      SizedBox(height: 45.0),
+                      emailField,
+                      SizedBox(height: 25.0),
+                      passwordField,
+                      SizedBox(
+                        height: 35.0,
+                      ),
+                      loginButon,
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+      )
     );
   }
 }
